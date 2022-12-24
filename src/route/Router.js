79bf4c3ad4623c8ee2571b52e77442607b902cb5
jyk2022 <foodgoder@kakao.com>
2022-12-24 14:04:42 +0900

    수정하였습니다

--- src/route/Router.js
+++ src/route/Router.js
@@ -1,11 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import Signup from "../pages/Signup";
-import Write from "../pages/Write";
 import Main from "../pages/Main";
 import Login from "../pages/Login";
-import Post from "../pages/Post";
 import Chat from "../pages/Chat";
-import PostList from "../pages/PostList";
+
 const Router = () => {
   return (
     <Routes>
@@ -13,9 +11,6 @@ const Router = () => {
       <Route path="/main" element={<Main />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
-      <Route path="/write" element={<Write />} />
-      <Route path="/postList" element={<PostList />} />
-      <Route path="/post" element={<Post />} />
       <Route path="/chat" element={<Chat />} />
       <Route path="*" element={<div>이거 404 페이지요</div>} />
     </Routes>
